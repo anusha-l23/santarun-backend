fa5153172993523970fb05577effee441b011189
anusha-l23 <anusha.l@leanfolks.com>
2023-12-23 13:51:17 +0530

    deleted unwanted code

--- controllers/EventControllers.js
+++ controllers/EventControllers.js
@@ -2,64 +2,6 @@ const {Event} = require("../models");
 const {CreateEvent} = require("../models");
 const multer = require('multer');
 
-// const selectEvent = async (req, res) => {
-//   try {
-//     const { eventName } = req.body;
-
-
-//     const existingEvent = await Event.findOne({ where: { eventName } });
-
-//     if (!existingEvent) {
-      
-//       const event = await Event.create(req.body);
-//       res.status(200).json(event);
-//     } else {
-  
-//       res.status(200).json(existingEvent);
-//     }
-//   } catch (error) {
-//     console.error(error, "error");
-//     res.status(500).json({ error: 'Internal Server Error' });
-//   }
-// }
-
-// const storage = multer.diskStorage({
-//   destination: function (req, file, cb) {
-//     cb(null, 'uploads/');
-//   },
-//   filename: function (req, file, cb) {
-//     cb(null, file.originalname);
-//   }
-// });
-
-// const upload = multer({ storage: storage });
-// const EventCreate = async(req, res) => {
-
-//   try {
-//     upload.single("eventPicture")(req, res, async (err)=> {
-// if(err) {
-//   console.error(err)
-//   return res.status(500).json({error: "File upload error"})
-// }
-
-//     const {eventName, location, year, categoryName, categoryAmount} = req.body;
-//     console.log(req.body, req.body);
-//     const eventPicture = req.file ? req.file.path : null;
-//     const eventCreate = await CreateEvent.create({
-//       eventName,
-//       location,
-//       year,
-//       eventPicture,
-//       categoryName, categoryAmount
-//     });
-//     res.status(200).json({message: "Event created successfully", eventCreate});
-//   });
-//   } catch (error) {
-//     console.error(error, "error");
-//     res.status(500).json({error: "Internal, server error"});
-//   }
-// }
-
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
     cb(null, 'uploads/');
@@ -98,6 +40,7 @@ try{
     console.log(categoryDetails, "categorydatails");
     console.log(req.body, "req.body")
     const eventPicture = req.file ? req.file.path : null;
+
     const eventCreate = await CreateEvent.create({
       eventName,
       location,
